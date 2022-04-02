3cbf2f644be304459a39ee5e0f88ab909343c7ad
ivanvikvik <ivanvikvik@gmail.com>
2022-04-02 19:30:05 +0300

    Made solutions more scalable by using java arrays and loop for

--- src/by/kursy/vikvik/javalessons/lesson12/task01/controller/Main.java
+++ src/by/kursy/vikvik/javalessons/lesson12/task01/controller/Main.java
@@ -5,18 +5,22 @@ import by.kursy.vikvik.javalessons.view.Printer;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Object {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Printer.print("Input student's marks: ");
-        int mark1 = scanner.nextInt();
-        int mark2 = scanner.nextInt();
-        int mark3 = scanner.nextInt();
-        int mark4 = scanner.nextInt();
-        int mark5 = scanner.nextInt();
-        int mark6 = scanner.nextInt();
+        Printer.print("Input count of students: ");
+        int count = scanner.nextInt();
 
-        double avg = Manager.calcAvgMark(mark1, mark2, mark3, mark4, mark5, mark6);
+        int[] marks = new int[count];
+
+        Printer.print("Input student's marks: ");
+
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = scanner.nextInt();
+        }
+
+        double avg = Manager.calcAvgMark(marks);
 
         String result = String.format("\nStudent's average mark = %.2f", avg);
         Printer.print(result);
--- src/by/kursy/vikvik/javalessons/lesson12/task01/model/Manager.java
+++ src/by/kursy/vikvik/javalessons/lesson12/task01/model/Manager.java
@@ -1,10 +1,14 @@
 package by.kursy.vikvik.javalessons.lesson12.task01.model;
 
 public class Manager {
-    public static final int MARK_COUNT = 6;
 
-    public static double calcAvgMark(int m1, int m2, int m3, int m4, int m5, int m6) {
-        double sum = m1 + m2 + m3 + m4 + m5 + m6;
-        return sum / MARK_COUNT;
+    public static double calcAvgMark(int[] marks) {
+        double sum = 0;
+
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+
+        return sum / marks.length;
     }
 }
