afe95dfb447836ed95deedea4b2d35cfe55de79a
ivanvikvik <ivanvikvik@gmail.com>
2022-04-02 18:31:30 +0300

    Solved the problem without using arrays

--- src/by/kursy/vikvik/javalessons/lesson12/task01/controller/Main.java
+++ src/by/kursy/vikvik/javalessons/lesson12/task01/controller/Main.java
@@ -1,4 +1,23 @@
 package by.kursy.vikvik.javalessons.lesson12.task01.controller;
 
+import by.kursy.vikvik.javalessons.lesson12.task01.model.Manager;
+import by.kursy.vikvik.javalessons.view.Printer;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Printer.print("Input student's marks: ");
+        int mark1 = scanner.nextInt();
+        int mark2 = scanner.nextInt();
+        int mark3 = scanner.nextInt();
+        int mark4 = scanner.nextInt();
+        int mark5 = scanner.nextInt();
+
+        double avg = Manager.calcAvgMark(mark1, mark2, mark3, mark4, mark5);
+
+        String result = String.format("\nStudent's average mark = %.2f", avg);
+        Printer.print(result);
+    }
 }
--- src/by/kursy/vikvik/javalessons/lesson12/task01/model/Manager.java
+++ src/by/kursy/vikvik/javalessons/lesson12/task01/model/Manager.java
@@ -1,4 +1,10 @@
 package by.kursy.vikvik.javalessons.lesson12.task01.model;
 
 public class Manager {
+    public static final int MARK_COUNT = 5;
+
+    public static double calcAvgMark(int m1, int m2, int m3, int m4, int m5) {
+        double sum = m1 + m2 + m3 + m4 + m5;
+        return sum / MARK_COUNT;
+    }
 }
