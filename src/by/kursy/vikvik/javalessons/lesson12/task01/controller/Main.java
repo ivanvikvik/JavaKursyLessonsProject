649f3d77e7c9b7b58e3bd68daf863c251255e4d0
ivanvikvik <ivanvikvik@gmail.com>
2022-04-02 18:52:03 +0300

    Changed solution for six marks

--- src/by/kursy/vikvik/javalessons/lesson12/task01/controller/Main.java
+++ src/by/kursy/vikvik/javalessons/lesson12/task01/controller/Main.java
@@ -14,8 +14,9 @@ public class Main {
         int mark3 = scanner.nextInt();
         int mark4 = scanner.nextInt();
         int mark5 = scanner.nextInt();
+        int mark6 = scanner.nextInt();
 
-        double avg = Manager.calcAvgMark(mark1, mark2, mark3, mark4, mark5);
+        double avg = Manager.calcAvgMark(mark1, mark2, mark3, mark4, mark5, mark6);
 
         String result = String.format("\nStudent's average mark = %.2f", avg);
         Printer.print(result);
--- src/by/kursy/vikvik/javalessons/lesson12/task01/model/Manager.java
+++ src/by/kursy/vikvik/javalessons/lesson12/task01/model/Manager.java
@@ -1,10 +1,10 @@
 package by.kursy.vikvik.javalessons.lesson12.task01.model;
 
 public class Manager {
-    public static final int MARK_COUNT = 5;
+    public static final int MARK_COUNT = 6;
 
-    public static double calcAvgMark(int m1, int m2, int m3, int m4, int m5) {
-        double sum = m1 + m2 + m3 + m4 + m5;
+    public static double calcAvgMark(int m1, int m2, int m3, int m4, int m5, int m6) {
+        double sum = m1 + m2 + m3 + m4 + m5 + m6;
         return sum / MARK_COUNT;
     }
 }
