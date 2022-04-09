a5aa2422544c29d1fb9a4ff7da67d228f46eceb0
ivanvikvik <ivanvikvik@gmail.com>
2022-04-09 18:55:04 +0300

    Add task05

---
+++ src/by/kursy/vikvik/javalessons/lesson13/task05/Main.java
@@ -0,0 +1,29 @@
+package by.kursy.vikvik.javalessons.lesson13.task05;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {4, 65, 7, 9, 5, 32};
+
+//        ArrayList list = new ArrayList();
+//        list.add(3);
+//        list.add(4);
+//        list.add(8);
+//        list.add(23);
+//        list.add(1);
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            int temp = (int)list.get(i);
+//        }
+
+        //        for (int i = 0; i < array.length; i++) {
+//            int temp = array[i];
+//        }
+
+        for (int temp : array) {
+            System.out.print(temp + " ");
+        }
+
+    }
+}
