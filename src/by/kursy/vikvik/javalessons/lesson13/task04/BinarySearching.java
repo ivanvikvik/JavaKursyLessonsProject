b5d4f4c043a392f19d1a3175f3560fcca842dca5
ivanvikvik <ivanvikvik@gmail.com>
2022-04-09 18:23:21 +0300

    Add BinarySearching class with binary searching algorithm

---
+++ src/by/kursy/vikvik/javalessons/lesson13/task04/BinarySearching.java
@@ -0,0 +1,21 @@
+package by.kursy.vikvik.javalessons.lesson13.task04;
+
+public class BinarySearching {
+    public static boolean checkValue(int[] array, int value) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (array[middle] == value) {
+                return true;
+            } else if (array[middle] < value) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return false;
+    }
+}
