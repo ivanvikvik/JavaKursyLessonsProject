abe763fd57727996ae09a1e9e37e63c0b69ca45b
ivanvikvik <ivanvikvik@gmail.com>
2022-04-16 19:40:54 +0300

    Add Selected sorting implementation

--- src/by/kursy/vikvik/javalessons/lesson14/ArraySorter.java
+++ src/by/kursy/vikvik/javalessons/lesson14/ArraySorter.java
@@ -60,4 +60,56 @@ public class ArraySorter {
 
         System.out.println("\nCount = " + count);
     }
+
+    public static void selectedSortAsc(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+//            //for best case
+//            boolean flag = true;
+//            for (int q = i; q < array.length - 1; q++ ) {
+//                if (array[q] > array[q + 1]) {
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//
+//            if (flag) {
+//                break;
+//            }
+
+            int index = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[index] > array[j]){
+                    index = j;
+                }
+                count++;
+            }
+
+            int t = array[i];
+            array[i] = array[index];
+            array[index] = t;
+        }
+        System.out.println("\nCount = " + count);
+    }
+
+    public static void selectedSortDesc(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+
+            int index = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[index] < array[j]){
+                    index = j;
+                }
+                count++;
+            }
+
+            int t = array[i];
+            array[i] = array[index];
+            array[index] = t;
+        }
+        System.out.println("\nCount = " + count);
+    }
 }
---
+++ src/by/kursy/vikvik/javalessons/lesson14/task03/Main.java
@@ -0,0 +1,23 @@
+package by.kursy.vikvik.javalessons.lesson14.task03;
+
+import by.kursy.vikvik.javalessons.lesson14.ArraySorter;
+import by.kursy.vikvik.javalessons.view.Printer;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] array = {1, 2, 3, 8, 6, 9, 5, 3, 10, 7};
+//        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] array = {10, 2, 3, 3, 5, 6, 7, 8, 9, 1};
+
+        Printer.print("Before sorting: ");
+        Printer.print(Arrays.toString(array));
+
+        ArraySorter.selectedSortAsc(array);
+
+        Printer.print("After sorting: ");
+        Printer.print(Arrays.toString(array));
+
+    }
+}
