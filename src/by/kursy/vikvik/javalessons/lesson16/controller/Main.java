d2a8d1f5a96048143e834232572e836d5b42d93f
ivanvikvik <ivanvikvik@gmail.com>
2022-05-07 18:46:48 +0300

    Add initialization blocks and new java keyword this

--- src/by/kursy/vikvik/javalessons/lesson16/controller/Main.java
+++ src/by/kursy/vikvik/javalessons/lesson16/controller/Main.java
@@ -6,13 +6,21 @@ import by.kursy.vikvik.javalessons.lesson16.model.entity.Student;
 // 1) JVM (default values)
 // 2) manual
 // 3) constructors (default, with args, ...)
-// 4)
-// 5)
+// 4) fields definition !!!
+// 5) init blocks
 
 public class Main {
 
     public static void main(String[] args) {
-        Student student = new Student("Alex", 20, 'm', true);
-        System.out.println(student.getInfo());
+        //Student st2 = new Student("Alex", 20, 'm', true);
+        //st2.name = "Alex";
+        Student st1 = new Student();
+        Student st2 = new Student("Alex", 20, 10, 'm', true);
+
+        System.out.println(st1.hashCode());
+        st1.testing(); //--> Student.testing(this = st1);
+        System.out.println();
+        System.out.println(st2.hashCode());
+        st2.testing();
     }
 }
--- src/by/kursy/vikvik/javalessons/lesson16/model/entity/Student.java
+++ src/by/kursy/vikvik/javalessons/lesson16/model/entity/Student.java
@@ -1,55 +1,83 @@
 package by.kursy.vikvik.javalessons.lesson16.model.entity;
 
 public class Student {
-    public String name;     //8
-    public int age;         //4
-    public double mark;     //8
-    public char sex;        //2    // 'm' or 'f'
-    public boolean alive;   //4
+    public static int count = 4;
+
+    {
+        this.mark = 4;
+        System.out.println("init block 2");
+    }
+
+
+    {
+        System.out.println("init block 1");
+    }
+
+    public String name;
+    public int age;
+    public double mark;
+    public char sex;
+    public boolean alive;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, double mark, char sex, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+        this.sex = sex;
+        this.alive = alive;
+    }
+
+    public void testing(){
+        System.out.println(this.hashCode());
+    }
 
     // default constructor
-    public Student() {
-        name = "no name";
-        age = 16;
-        mark = 4;
-        alive = true;
-        sex = 'f';
-    }
+//    public Student() {
+//        name = "no name";
+//        age = 16;
+//        alive = true;
+//        sex = 'f';
+//    }
 
     // constructor with args (params)
-    public Student(String n, int a, double m, char s, boolean al) {
-        name = n;
-        age = a;
-        mark = m;
-        sex = s;
-        alive = al;
+//    public Student(String name, int age, double mark, char sex, boolean alive) {
+//        this.name = name;
+//        this.age = age;
+//        this.mark = mark;
+//        this.sex = sex;
+//        this.alive = alive;
+//    }
+
+//    // constructor with args (params)
+//    public Student(String name) {
+//        this.name = name;
+//    }
+//
+//    // constructor with args (params)
+//    public Student(String name, int age, char sex, boolean alive) {
+//        this.name = name;
+//        this.age = age;
+//        this.sex = sex;
+//        this.alive = alive;
+//    }
+//
+//    // constructor with args (params)
+//    public Student(boolean alive) {
+//        this.alive = alive;
+//    }
+
+    public void setName(String name){
+        this.name = name;
     }
-
-    // constructor with args (params)
-    public Student(String n) {
-        name = n;
-    }
-
-    // constructor with args (params)
-    public Student(String n, int a, char s, boolean al) {
-        name = n;
-        age = a;
-        sex = s;
-        alive = al;
-    }
-
-    // constructor with args (params)
-    public Student(boolean al) {
-        alive = al;
-    }
-
-
 
 
     public String getInfo() {
-        return name + " { age = " + age
-                + "; mark = " + mark
-                + "; sex = " + (sex == 'm' ? "male" : "female")
-                + "; alive = " + (alive ? "yes" : "no") + " }";
+        return this.name + " { age = " + this.age
+                + "; mark = " + this.mark
+                + "; sex = " + (this.sex == 'm' ? "male" : "female")
+                + "; alive = " + (this.alive ? "yes" : "no") + " }";
     }
 }
