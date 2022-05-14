5ed9dc0dae3646127bf8336077b56867706372eb
ivanvikvik <ivanvikvik@gmail.com>
2022-05-14 19:30:12 +0300

    Using first step inheritance as code reuse

--- src/by/kursy/vikvik/javalessons/lesson17/controller/Main.java
+++ src/by/kursy/vikvik/javalessons/lesson17/controller/Main.java
@@ -3,30 +3,9 @@ package by.kursy.vikvik.javalessons.lesson17.controller;
 import by.kursy.vikvik.javalessons.lesson17.model.Student;
 
 public class Main {
-    public static int test(int n)
-    {
-        n += 10;
-        return n;
-    }
-
-    public static Student test(Student s)
-    {
-        s = new Student("Alex", 19, 9, true);
-        return s;
-    }
-
     public static void main(String[] args) {
-//        int x = 10;
-//        System.out.println("Before x = " + x);
-//        x = test(x);
-//        System.out.println("After x = " + x);
-
-        System.out.println(-17 | 0b10000);
-        Student s = new Student();
-        s.setName("Kate");
-        System.out.println("Before name = " + s.getName());
-        s = test(s);
-        System.out.println("After name = " + s.getName());
+        Student s;
+        s.
 
     }
 }
--- src/by/kursy/vikvik/javalessons/lesson17/model/Student.java
+++ src/by/kursy/vikvik/javalessons/lesson17/model/Student.java
@@ -1,6 +1,6 @@
 package by.kursy.vikvik.javalessons.lesson17.model;
 
-public class Student {
+public class Student extends Human {
 
     public static final int MIN_STUDENT_AGE = 16;
     public static final int MAX_STUDENT_AGE = 16;
@@ -8,41 +8,21 @@ public class Student {
     public static final int MIN_MARK = 0;
     public static final int MAX_MARK = 10;
 
-    private String name;
-    private int age;
     private double mark;
     private boolean alive;
 
     public Student() {
-        name = "no name";
-        age = 16;
+        setName("no name");
+        setAge(16);
         mark = 4;
         alive = true;
     }
 
     public Student(String name, int age, double mark, boolean alive) {
-        this.name = name;
-        this.age = age;
+        setName(name);
+        setAge(age);
         this.mark = mark;
         this.alive = alive;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age >= MIN_STUDENT_AGE && age <= MAX_STUDENT_AGE) {
-            this.age = age;
-        }
     }
 
     public double getMark() {
@@ -65,7 +45,7 @@ public class Student {
 
     @Override
     public String toString() {
-        return name + ": age = " + age
+        return getName() + ": age = " + getAge()
                 + ", mark = " + mark
                 + ", is alive = " + (alive ? "yes" : "no");
     }
