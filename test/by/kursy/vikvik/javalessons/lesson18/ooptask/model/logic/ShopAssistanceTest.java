e16160918e1e09c3e685777e65acb0a7172c6b21
ivanvikvik <ivanvikvik@gmail.com>
2022-05-28 17:46:57 +0300

    Update lesson18 test-cases with using init-block

--- test/by/kursy/vikvik/javalessons/lesson18/ooptask/model/logic/ShopAssistanceTest.java
+++ test/by/kursy/vikvik/javalessons/lesson18/ooptask/model/logic/ShopAssistanceTest.java
@@ -3,26 +3,29 @@ package by.kursy.vikvik.javalessons.lesson18.ooptask.model.logic;
 import by.kursy.vikvik.javalessons.lesson18.ooptask.model.entity.Bouquet;
 import by.kursy.vikvik.javalessons.lesson18.ooptask.model.entity.Flower;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
 public class ShopAssistanceTest {
 
-    @Test
-    public void testCalculateTotalPricePositive() {
+    private Bouquet bouquet;
+
+    {
         Flower[] flowers = {
                 new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
+                new Flower("Rose", "red", 3.0, 100, 130),
+                new Flower("Rose", "red", 3.0, 100, 75),
+                new Flower("Rose", "red", 3.0, 100, 120),
+                new Flower("Rose", "red", 3.0, 100, 110),
         };
-        Bouquet bouquet = new Bouquet(flowers);
+        bouquet = new Bouquet(flowers);
+    }
 
+    @Test
+    public void testCalculateTotalPricePositive() {
         double expected = (ShopAssistance.LABOR_PERCENT + 1) * 15;
-
         double actual = ShopAssistance.calculateTotalPrice(bouquet);
-
         assertEquals(expected, actual, 0.01);
     }
 
@@ -38,7 +41,7 @@ public class ShopAssistanceTest {
     @Test
     public void testCalculateTotalPriceWithEmptyObject() {
         Flower[] flowers = {};
-        Bouquet bouquet = new Bouquet(flowers);
+        bouquet = new Bouquet(flowers);
 
         double expected = 0;
 
@@ -49,86 +52,46 @@ public class ShopAssistanceTest {
 
     @Test
     public void testCalculateTotalWeightPositive() {
-        Flower[] flowers = {
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 100),
-        };
-
-        Bouquet bouquet = new Bouquet(flowers);
-
         double expected = 500;
-
         double actual = ShopAssistance.calculateTotalWeight(bouquet);
-
         assertEquals(expected, actual, 0.1);
     }
 
     @Test
     public void testCalculateTotalWeightWithNull() {
         double expected = 0;
-
         double actual = ShopAssistance.calculateTotalWeight(null);
-
         assertEquals(expected, actual, 0.0);
     }
 
     @Test
     public void testCalculateTotalWeightWithEmptyObject() {
         Flower[] flowers = {};
-        Bouquet bouquet = new Bouquet(flowers);
-
+        bouquet = new Bouquet(flowers);
         double expected = 0;
-
         double actual = ShopAssistance.calculateTotalWeight(bouquet);
-
         assertEquals(expected, actual, 0.0);
     }
 
     @Test
-    public void firstTestFindFlowersWithMaxLengthPositive(){
-        Flower[] flowers = {
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 130),
-                new Flower("Rose", "red", 3.0, 100, 75),
-                new Flower("Rose", "red", 3.0, 100, 120),
-                new Flower("Rose", "red", 3.0, 100, 110),
-        };
-
-        Bouquet bouquet = new Bouquet(flowers);
-
+    public void firstTestFindFlowersWithMaxLengthPositive() {
         Flower[] expected = {new Flower("Rose", "red", 3.0, 100, 130)};
-
         Flower[] actual = ShopAssistance.findFlowersWithMaxLength(bouquet);
-
         //assertArrayEquals(expected, actual);
         for (int i = 0; i < expected.length; i++) {
             if (expected[i].getLength() != actual[i].getLength()
-            || expected[i].getWeight() != actual[i].getWeight()
-            || expected[i].getPrice() != actual[i].getPrice()
-            || !expected[i].getName().equals(actual[i].getName())
-            || !expected[i].getColor().equals(actual[i].getColor())) {
+                    || expected[i].getWeight() != actual[i].getWeight()
+                    || expected[i].getPrice() != actual[i].getPrice()
+                    || !expected[i].getName().equals(actual[i].getName())
+                    || !expected[i].getColor().equals(actual[i].getColor())) {
                 fail();
             }
         }
     }
 
     @Test
-    public void secondTestFindFlowersWithMaxLengthPositive(){
-        Flower[] flowers = {
-                new Flower("Rose", "red", 3.0, 100, 100),
-                new Flower("Rose", "red", 3.0, 100, 130),
-                new Flower("Rose", "red", 3.0, 100, 75),
-                new Flower("Rose", "red", 3.0, 100, 130),
-                new Flower("Rose", "red", 3.0, 100, 110),
-        };
-
-        Bouquet bouquet = new Bouquet(flowers);
-
+    public void secondTestFindFlowersWithMaxLengthPositive() {
         Flower[] expected = {new Flower("Rose", "red", 3.0, 100, 130)};
-
         Flower[] actual = ShopAssistance.findFlowersWithMaxLength(bouquet);
 
         //assertArrayEquals(expected, actual);
