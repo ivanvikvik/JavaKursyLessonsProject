91423a2f02ea1b60601f7050b926b2e6fff3b35e
ivanvikvik <ivanvikvik@gmail.com>
2022-07-22 22:08:47 +0300

    Use Input stream for reading integer numbers from file text.bin

--- src/by/kursy/vikvik/javalessons/lesson28/controller/Main.java
+++ src/by/kursy/vikvik/javalessons/lesson28/controller/Main.java
@@ -5,6 +5,7 @@ import by.kursy.vikvik.javalessons.lesson28.model.BinaryWorker;
 public class Main {
     public static void main(String[] args) {
         String fileName = "c:/test/test.bin";
-        BinaryWorker.write(fileName);
+//        BinaryWorker.write(fileName);
+        System.out.println(BinaryWorker.read(fileName));
     }
 }
--- src/by/kursy/vikvik/javalessons/lesson28/model/BinaryWorker.java
+++ src/by/kursy/vikvik/javalessons/lesson28/model/BinaryWorker.java
@@ -1,8 +1,6 @@
 package by.kursy.vikvik.javalessons.lesson28.model;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class BinaryWorker {
     public static void write(String fileName) {
@@ -28,5 +26,53 @@ public class BinaryWorker {
             }
         }
 
+    }
+
+    public static String read(String fileName) {
+        InputStream stream = null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            stream = new FileInputStream(fileName);
+
+            int number;
+
+            while((number = stream.read()) != -1){
+                builder.append(number).append(" ");
+            }
+
+            // 2
+//            int number = stream.read();
+//
+//            while(number != -1){
+//                builder.append(number).append(" ");
+//                number = stream.read();
+//            }
+
+            // 3
+//            while(stream.available() > 0){
+//                int number = stream.read();
+//                builder.append(number).append(" ");
+//            }
+
+            //4
+//            int size = stream.available();
+//            for (int i = 0; i < size; i++) {
+//                builder.append(stream.read()).append(" ");
+//            }
+
+        } catch (IOException exception) {
+            System.err.println(exception);
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (IOException exception) {
+                System.err.println(exception);
+            }
+        }
+
+        return builder.toString();
     }
 }
